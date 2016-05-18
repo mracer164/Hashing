a35b9f7258bbff6d6d97d6c3e95ea0026dd6a6b3
Mateusz <Mateusz@DESKTOP-TTJO1B8>
2016-05-18 20:57:12 +0200

    Added function filling Hashtable

--- Hashing/Hashing/Source.cpp
+++ Hashing/Hashing/Source.cpp
@@ -1,8 +1,71 @@
 #include <iostream>
+#include <random>
 
 using namespace std;
-const int TABLE_SIZE = 1000000;
+const int TABLE_SIZE = 20;
+unsigned long * hashTable = new unsigned long[TABLE_SIZE];
 unsigned long x = 123456789, y = 362436069, z = 521288629;
+unsigned long randomNumberGenerator()
+{
+	unsigned long t;
+	x ^= x << 16;
+	x ^= x >> 5;
+	x ^= x << 1;
+
+	t = x;
+	x = y;
+	y = z;
+	z = t ^ x ^ y;
+
+	return z;
+}
+int randomWithinRange(int lowerBound, int upperBound)
+{
+	random_device rd; 
+	mt19937 eng(rd()); 
+	uniform_int_distribution<> distr(lowerBound, upperBound); 
+
+	return distr(eng);
+}
+void fillHashTable(int fillPercentage, unsigned long * hashTable)
+{
+	
+	bool repeated = false;
+	int currentIndex = 0;
+	int lastFilledIndex = 0;
+	int fillingProgress = 0;
+	int upperBound = TABLE_SIZE*fillPercentage / 100;
+	int * generatedNumbers = new int[upperBound];
+	int generatedNumbersIterator = 0;
+	while (fillingProgress <= upperBound)
+	{
+		currentIndex = randomWithinRange(0, TABLE_SIZE);
+		generatedNumbers[generatedNumbersIterator] = currentIndex;
+		for (int i = 0; i <= lastFilledIndex; i++)
+		{
+			if (generatedNumbers[i] == currentIndex && generatedNumbersIterator != i)
+			{
+				repeated = true;
+			}
+			else
+			{
+				generatedNumbersIterator++;
+			}
+		}
+		if (repeated == false)
+		{
+			hashTable[currentIndex] = randomNumberGenerator();
+			fillingProgress++;
+			repeated = false;
+		}
+		else
+		{
+			repeated = false;
+			continue;
+		}
+
+	}
+}
 int hashInt(int toHash, int i)
 {
 	int hash;
@@ -43,23 +106,14 @@ bool hash_lin_prob_search(int x, int * hashTable)
 	}
 	return false;
 }
-int randomNumberGenerator()
-{
-	unsigned long t;
-	x ^= x << 16;
-	x ^= x >> 5;
-	x ^= x << 1;
 
-	t = x;
-	x = y;
-	y = z;
-	z = t ^ x ^ y;
-
-	return z;
-}
 
 int main()
 {
-
+	fillHashTable(25, hashTable);
+	for (int i = 0; i < TABLE_SIZE; i++)
+	{
+		cout << hashTable[i] << endl;
+	}
 	return 0;
 }
